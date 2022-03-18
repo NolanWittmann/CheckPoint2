49538a694df6eea1182d321f61a3198cb4296fcb
Nolan Wittmann <wittmann.nolan@gmail.com>
2022-03-18 09:58:15 +0100

    starting1

--- react_game_checkpoint2/src/App.js
+++ react_game_checkpoint2/src/App.js
@@ -1,9 +1,12 @@
+import Header from "./components/Header";
 
 
 function App() {
+
+  const name = "REACT-GAMES_CHECKPOINT-2"
   return (
     <div className="App">
-
+      <Header name={name} />
     </div>
   );
 }
